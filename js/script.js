bc1c4e76622f9d2123d8da85f07ee48c58fef2cc
Magdalena Staliś <magdalenastalis@gmail.com>
2018-05-15 19:10:26 +0200

    Finish html/css brand section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-// $('.slider').slick({
-//   infinite: true,
-//   slidesToShow: 3,
-//   slidesToScroll: 3
-// });
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000
+});
+
+
 function showSection(button, section) {
     section.classList.remove("hidden");
     section.classList.add("shown");
@@ -19,32 +23,57 @@ function hideSection(button, section) {
 }
 
 
-window.onload = function() {
-
+window.onload = function () {
+    //section offer
     var repairsButton = document.getElementById("repairs-button");
     var installationButton = document.getElementById("installation-button");
     var salesButton = document.getElementById("sales-button");
     var repairs = document.getElementById("repairs");
     var installation = document.getElementById("installation");
     var sales = document.getElementById("sales");
-    
-    
-    salesButton.addEventListener("click", function(){
+
+
+    salesButton.addEventListener("click", function () {
         showSection(salesButton, sales);
         hideSection(repairsButton, repairs);
         hideSection(installationButton, installation);
-    }); 
+    });
 
-    repairsButton.addEventListener("click", function(){
+    repairsButton.addEventListener("click", function () {
         showSection(repairsButton, repairs);
         hideSection(salesButton, sales);
         hideSection(installationButton, installation);
-    }); 
+    });
 
-    installationButton.addEventListener("click", function(){
+    installationButton.addEventListener("click", function () {
         showSection(installationButton, installation);
         hideSection(repairsButton, repairs);
         hideSection(salesButton, sales);
-    }); 
+    });
+
+    //section brands
+
+    var otherBrands = document.getElementById("other-brands");
+    var buttonBrands = document.getElementById("button-brands");
+
+    buttonBrands.addEventListener("click", function () {
+        if (buttonBrands.innerText.toLowerCase === "Pokaż więcej marek".toLowerCase) {
+            otherBrands.classList.remove("hidden");
+            otherBrands.classList.add("shown");
+            buttonBrands.innerText = "Ukryj więcej marek";
+        }
+        else {
+            otherBrands.classList.add("hidden");
+            otherBrands.classList.remove("shown");
+            buttonBrands.innerText = "Pokaż więcej marek";
+        }
+
+
+    });
+
+
+
 
 };
+
+
